74f7846c51d9571db9e5967fecb2da39082e8424
Harini <harinivenkatesh96@gmail.com>
2026-07-17 21:29:31 +0530

    File updated at 17/07/2026, 21:29:32

--- 441-ArrangingCoins/441-ArrangingCoins.java
+++ 441-ArrangingCoins/441-ArrangingCoins.java
@@ -1,25 +1,18 @@
-// Last updated: 17/07/2026, 21:25:59
+// Last updated: 17/07/2026, 21:29:32
 1class Solution {
-2    public String licenseKeyFormatting(String s, int k) {
-3        StringBuilder sb = new StringBuilder();
-4        int count = 0;
+2    public int findMaxConsecutiveOnes(int[] nums) {
+3        int count = 0;
+4        int max = 0;
 5
-6        for (int i = s.length() - 1; i >= 0; i--) {
-7            char ch = s.charAt(i);
-8
-9            if (ch == '-') {
-10                continue;
-11            }
-12
-13            if (count == k) {
-14                sb.append('-');
-15                count = 0;
-16            }
-17
-18            sb.append(Character.toUpperCase(ch));
-19            count++;
-20        }
-21
-22        return sb.reverse().toString();
-23    }
-24}
+6        for (int num : nums) {
+7            if (num == 1) {
+8                count++;
+9                max = Math.max(max, count);
+10            } else {
+11                count = 0;
+12            }
+13        }
+14
+15        return max;
+16    }
+17}
