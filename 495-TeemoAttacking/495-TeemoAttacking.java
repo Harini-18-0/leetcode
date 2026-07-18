14b80bdee0c940326a4aaf06e2549b2e6d7eecfb
Harini <harinivenkatesh96@gmail.com>
2026-07-18 21:55:03 +0530

    File updated at 18/07/2026, 21:55:04

--- 495-TeemoAttacking/495-TeemoAttacking.java
+++ 495-TeemoAttacking/495-TeemoAttacking.java
@@ -1,16 +1,42 @@
-// Last updated: 18/07/2026, 21:53:56
-1class Solution {
-2    public int findPoisonedDuration(int[] timeSeries, int duration) {
-3        
-4        int total = 0;
+// Last updated: 18/07/2026, 21:55:04
+1import java.util.*;
+2
+3class Solution {
+4    public String[] findWords(String[] words) {
 5        
-6        for (int i = 0; i < timeSeries.length - 1; i++) {
-7            total += Math.min(duration, timeSeries[i + 1] - timeSeries[i]);
-8        }
+6        String row1 = "qwertyuiop";
+7        String row2 = "asdfghjkl";
+8        String row3 = "zxcvbnm";
 9        
-10        // Add poison duration for the last attack
-11        total += duration;
-12        
-13        return total;
-14    }
-15}
+10        List<String> result = new ArrayList<>();
+11        
+12        for (String word : words) {
+13            String w = word.toLowerCase();
+14            
+15            String row = "";
+16            
+17            if (row1.indexOf(w.charAt(0)) != -1) {
+18                row = row1;
+19            } else if (row2.indexOf(w.charAt(0)) != -1) {
+20                row = row2;
+21            } else {
+22                row = row3;
+23            }
+24            
+25            boolean valid = true;
+26            
+27            for (char c : w.toCharArray()) {
+28                if (row.indexOf(c) == -1) {
+29                    valid = false;
+30                    break;
+31                }
+32            }
+33            
+34            if (valid) {
+35                result.add(word);
+36            }
+37        }
+38        
+39        return result.toArray(new String[0]);
+40    }
+41}
