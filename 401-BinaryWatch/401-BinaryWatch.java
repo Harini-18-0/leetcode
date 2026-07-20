be80b6f18d1b0dfc3ae6260a4bca48e581f1f404
Harini <harinivenkatesh96@gmail.com>
2026-07-20 18:14:23 +0530

    File updated at 20/07/2026, 18:14:25

--- 401-BinaryWatch/401-BinaryWatch.java
+++ 401-BinaryWatch/401-BinaryWatch.java
@@ -1,27 +1,20 @@
-// Last updated: 20/07/2026, 18:08:19
+// Last updated: 20/07/2026, 18:14:25
 1class Solution {
-2    public int longestPalindrome(String s) {
-3        int[] count = new int[128];
-4        for (char c : s.toCharArray()) {
-5            count[c]++;
-6        }
-7
-8        int length = 0;
-9        boolean oddFound = false;
-10
-11        for (int freq : count) {
-12            if (freq % 2 == 0) {
-13                length += freq;
-14            } else {
-15                length += freq - 1;
-16                oddFound = true;
-17            }
-18        }
-19
-20        if (oddFound) {
-21            length++;
-22        }
-23
-24        return length;
-25    }
-26}
+2    public List<String> fizzBuzz(int n) {
+3        List<String> answer = new ArrayList<>();
+4
+5        for (int i = 1; i <= n; i++) {
+6            if (i % 15 == 0) {
+7                answer.add("FizzBuzz");
+8            } else if (i % 3 == 0) {
+9                answer.add("Fizz");
+10            } else if (i % 5 == 0) {
+11                answer.add("Buzz");
+12            } else {
+13                answer.add(String.valueOf(i));
+14            }
+15        }
+16
+17        return answer;
+18    }
+19}
