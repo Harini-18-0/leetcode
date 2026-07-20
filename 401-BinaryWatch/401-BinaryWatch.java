16a40a9cc2295757a8582769daa85a4bc9a526a1
Harini <harinivenkatesh96@gmail.com>
2026-07-20 18:06:37 +0530

    File updated at 20/07/2026, 18:06:38

--- 401-BinaryWatch/401-BinaryWatch.java
+++ 401-BinaryWatch/401-BinaryWatch.java
@@ -1,15 +1,20 @@
-// Last updated: 20/07/2026, 18:00:40
+// Last updated: 20/07/2026, 18:06:38
 1class Solution {
-2    public List<String> readBinaryWatch(int turnedOn) {
-3        List<String> result = new ArrayList<>();
-4            for (int hour = 0; hour < 12; hour++) {
-5            for (int minute = 0; minute < 60; minute++) {
-6                if (Integer.bitCount(hour) + Integer.bitCount(minute) == turnedOn) {                   
-7                    result.add(hour + ":" + String.format("%02d", minute));
-8                }
-9            }
-10        }
-11        
-12        return result;
-13    }
-14}
+2    public String toHex(int num) {
+3        if (num == 0) return "0";
+4
+5        char[] hex = {
+6            '0','1','2','3','4','5','6','7',
+7            '8','9','a','b','c','d','e','f'
+8        };
+9
+10        StringBuilder sb = new StringBuilder();
+11
+12        while (num != 0) {
+13            sb.append(hex[num & 15]);
+14            num >>>= 4;               
+15        }
+16
+17        return sb.reverse().toString();
+18    }
+19}
