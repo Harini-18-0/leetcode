a3222c3fa441b308a7ac8f5008d5d41b8419813e
Harini <harinivenkatesh96@gmail.com>
2026-07-21 15:25:16 +0530

    File updated at 21/07/2026, 15:25:16

--- 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.java
+++ 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.java
@@ -1,35 +1,22 @@
-// Last updated: 21/07/2026, 15:24:32
+// Last updated: 21/07/2026, 15:25:16
 1class Solution {
-2    public String[] findRelativeRanks(int[] score) {
-3        int n = score.length;
-4        String[] answer = new String[n];
-5
-6        int[][] arr = new int[n][2];
-7
-8        // Store score and original index
-9        for (int i = 0; i < n; i++) {
-10            arr[i][0] = score[i];
-11            arr[i][1] = i;
-12        }
-13
-14        // Sort by score in descending order
-15        Arrays.sort(arr, (a, b) -> b[0] - a[0]);
-16
-17        // Assign ranks
-18        for (int i = 0; i < n; i++) {
-19            int index = arr[i][1];
-20
-21            if (i == 0) {
-22                answer[index] = "Gold Medal";
-23            } else if (i == 1) {
-24                answer[index] = "Silver Medal";
-25            } else if (i == 2) {
-26                answer[index] = "Bronze Medal";
-27            } else {
-28                answer[index] = String.valueOf(i + 1);
-29            }
-30        }
-31
-32        return answer;
-33    }
-34}
+2    public boolean checkPerfectNumber(int num) {
+3        if (num <= 1) {
+4            return false;
+5        }
+6
+7        int sum = 1;
+8
+9        for (int i = 2; i * i <= num; i++) {
+10            if (num % i == 0) {
+11                sum += i;
+12
+13                if (i != num / i) {
+14                    sum += num / i;
+15                }
+16            }
+17        }
+18
+19        return sum == num;
+20    }
+21}
