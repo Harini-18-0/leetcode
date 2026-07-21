0a20bb958f4ad729651acf4e08e0c72baac907cc
Harini <harinivenkatesh96@gmail.com>
2026-07-21 15:26:25 +0530

    File updated at 21/07/2026, 15:26:26

--- 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.java
+++ 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.java
@@ -1,22 +1,10 @@
-// Last updated: 21/07/2026, 15:25:16
+// Last updated: 21/07/2026, 15:26:26
 1class Solution {
-2    public boolean checkPerfectNumber(int num) {
-3        if (num <= 1) {
-4            return false;
+2    public int findLUSlength(String a, String b) {
+3        if (a.equals(b)) {
+4            return -1;
 5        }
 6
-7        int sum = 1;
-8
-9        for (int i = 2; i * i <= num; i++) {
-10            if (num % i == 0) {
-11                sum += i;
-12
-13                if (i != num / i) {
-14                    sum += num / i;
-15                }
-16            }
-17        }
-18
-19        return sum == num;
-20    }
-21}
+7        return Math.max(a.length(), b.length());
+8    }
+9}
