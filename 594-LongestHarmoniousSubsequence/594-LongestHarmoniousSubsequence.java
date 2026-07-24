8d4049f121dd1c494366dbe974ce9515fadd5de2
Harini <harinivenkatesh96@gmail.com>
2026-07-24 19:31:36 +0530

    File updated at 24/07/2026, 19:31:37

--- 594-LongestHarmoniousSubsequence/594-LongestHarmoniousSubsequence.java
+++ 594-LongestHarmoniousSubsequence/594-LongestHarmoniousSubsequence.java
@@ -1,33 +1,33 @@
-// Last updated: 24/07/2026, 19:30:26
-1import java.util.*;
-2
-3class Solution {
-4    public String[] findRestaurant(String[] list1, String[] list2) {
-5        HashMap<String, Integer> map = new HashMap<>();
+// Last updated: 24/07/2026, 19:31:37
+1class Solution {
+2    public int findIntegers(int n) {
+3        int[] dp = new int[32];
+4        dp[0] = 1;
+5        dp[1] = 2;
 6
-7        // Store strings from list1 with their indices
-8        for (int i = 0; i < list1.length; i++) {
-9            map.put(list1[i], i);
+7        // Build Fibonacci-like DP
+8        for (int i = 2; i < 32; i++) {
+9            dp[i] = dp[i - 1] + dp[i - 2];
 10        }
 11
-12        List<String> result = new ArrayList<>();
-13        int minSum = Integer.MAX_VALUE;
+12        int ans = 0;
+13        int prevBit = 0;
 14
-15        // Traverse list2
-16        for (int j = 0; j < list2.length; j++) {
-17            if (map.containsKey(list2[j])) {
-18                int sum = j + map.get(list2[j]);
+15        // Traverse bits from MSB to LSB
+16        for (int i = 30; i >= 0; i--) {
+17            if ((n & (1 << i)) != 0) {
+18                ans += dp[i];
 19
-20                if (sum < minSum) {
-21                    minSum = sum;
-22                    result.clear();
-23                    result.add(list2[j]);
-24                } else if (sum == minSum) {
-25                    result.add(list2[j]);
-26                }
+20                if (prevBit == 1) {
+21                    return ans;
+22                }
+23
+24                prevBit = 1;
+25            } else {
+26                prevBit = 0;
 27            }
 28        }
 29
-30        return result.toArray(new String[0]);
+30        return ans + 1;
 31    }
 32}
