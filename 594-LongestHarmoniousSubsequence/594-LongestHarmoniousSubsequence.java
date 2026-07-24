c0f060a696c03e10828e5652bfb99e21e8f6c123
Harini <harinivenkatesh96@gmail.com>
2026-07-24 19:29:08 +0530

    File updated at 24/07/2026, 19:29:09

--- 594-LongestHarmoniousSubsequence/594-LongestHarmoniousSubsequence.java
+++ 594-LongestHarmoniousSubsequence/594-LongestHarmoniousSubsequence.java
@@ -1,21 +1,18 @@
-// Last updated: 24/07/2026, 19:27:15
-1import java.util.HashMap;
-2
-3class Solution {
-4    public int findLHS(int[] nums) {
-5        HashMap<Integer, Integer> map = new HashMap<>();
-6        for (int num : nums) {
-7            map.put(num, map.getOrDefault(num, 0) + 1);
-8        }
+// Last updated: 24/07/2026, 19:29:09
+1class Solution {
+2    public int maxCount(int m, int n, int[][] ops) {
+3        if (ops.length == 0) {
+4            return m * n;
+5        }
+6
+7        int minA = m;
+8        int minB = n;
 9
-10        int maxLen = 0;
-11
-12        for (int key : map.keySet()) {
-13            if (map.containsKey(key + 1)) {
-14                maxLen = Math.max(maxLen, map.get(key) + map.get(key + 1));
-15            }
-16        }
-17
-18        return maxLen;
-19    }
-20}
+10        for (int[] op : ops) {
+11            minA = Math.min(minA, op[0]);
+12            minB = Math.min(minB, op[1]);
+13        }
+14
+15        return minA * minB;
+16    }
+17}
