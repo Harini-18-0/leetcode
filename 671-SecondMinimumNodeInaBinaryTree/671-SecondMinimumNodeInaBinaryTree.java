3b2aa9db68139a1fa3fa13d60b9408ef149aa8a6
Harini <harinivenkatesh96@gmail.com>
2026-07-25 21:14:58 +0530

    File updated at 25/07/2026, 21:14:59

--- 671-SecondMinimumNodeInaBinaryTree/671-SecondMinimumNodeInaBinaryTree.java
+++ 671-SecondMinimumNodeInaBinaryTree/671-SecondMinimumNodeInaBinaryTree.java
@@ -1,37 +1,23 @@
-// Last updated: 25/07/2026, 21:14:23
+// Last updated: 25/07/2026, 21:14:59
 1class Solution {
-2    public String decodeString(String s) {
-3        Stack<Integer> countStack = new Stack<>();
-4        Stack<StringBuilder> stringStack = new Stack<>();
-5
-6        StringBuilder current = new StringBuilder();
-7        int num = 0;
-8
-9        for (char ch : s.toCharArray()) {
-10            if (Character.isDigit(ch)) {
-11                num = num * 10 + (ch - '0');
-12            } 
-13            else if (ch == '[') {
-14                countStack.push(num);
-15                stringStack.push(current);
-16                num = 0;
-17                current = new StringBuilder();
-18            } 
-19            else if (ch == ']') {
-20                int repeat = countStack.pop();
-21                StringBuilder prev = stringStack.pop();
-22
-23                for (int i = 0; i < repeat; i++) {
-24                    prev.append(current);
-25                }
-26
-27                current = prev;
-28            } 
-29            else {
-30                current.append(ch);
-31            }
-32        }
-33
-34        return current.toString();
-35    }
-36}
+2    public int findNthDigit(int n) {
+3        long digits = 1;
+4        long count = 9;
+5        long start = 1;
+6
+7        // Find the digit-length group
+8        while (n > digits * count) {
+9            n -= digits * count;
+10            digits++;
+11            count *= 10;
+12            start *= 10;
+13        }
+14
+15        // Find the actual number
+16        start += (n - 1) / digits;
+17
+18        // Convert number to string and get the digit
+19        String num = Long.toString(start);
+20        return num.charAt((int)((n - 1) % digits)) - '0';
+21    }
+22}
