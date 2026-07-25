e618fe1a5b08ef46c4b0bf772d8580e7493cbb5e
Harini <harinivenkatesh96@gmail.com>
2026-07-25 21:13:17 +0530

    File updated at 25/07/2026, 21:13:18

--- 671-SecondMinimumNodeInaBinaryTree/671-SecondMinimumNodeInaBinaryTree.java
+++ 671-SecondMinimumNodeInaBinaryTree/671-SecondMinimumNodeInaBinaryTree.java
@@ -1,38 +1,18 @@
-// Last updated: 25/07/2026, 21:12:44
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    private long second = Long.MAX_VALUE;
-18    private int min;
-19
-20    public int findSecondMinimumValue(TreeNode root) {
-21        min = root.val;
-22        dfs(root);
-23
-24        return second == Long.MAX_VALUE ? -1 : (int) second;
-25    }
-26
-27    private void dfs(TreeNode node) {
-28        if (node == null) return;
-29
-30        if (node.val > min && node.val < second) {
-31            second = node.val;
-32        }
-33
-34        dfs(node.left);
-35        dfs(node.right);
-36    }
-37}
+// Last updated: 25/07/2026, 21:13:18
+1class Solution {
+2    public int findLengthOfLCIS(int[] nums) {
+3        int max = 1;
+4        int count = 1;
+5
+6        for (int i = 1; i < nums.length; i++) {
+7            if (nums[i] > nums[i - 1]) {
+8                count++;
+9            } else {
+10                count = 1;
+11            }
+12            max = Math.max(max, count);
+13        }
+14
+15        return max;
+16    }
+17}
