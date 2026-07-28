7e967112a5af1ed9f94e0c19177011599acaede0
Harini <harinivenkatesh96@gmail.com>
2026-07-28 20:32:50 +0530

    File updated at 28/07/2026, 20:32:50

--- 590-N-aryTreePostorderTraversal/590-N-aryTreePostorderTraversal.java
+++ 590-N-aryTreePostorderTraversal/590-N-aryTreePostorderTraversal.java
@@ -1,37 +1,31 @@
-// Last updated: 28/07/2026, 20:31:42
-1/*
-2 // Definition for a Node.
-3 class Node {
-4     public int val;
-5     public List<Node> children;
-6
-7     public Node() {}
-8
-9     public Node(int _val) {
-10         val = _val;
-11     }
-12
-13     public Node(int _val, List<Node> _children) {
-14         val = _val;
-15         children = _children;
-16     }
-17 };
-18*/
-19
-20class Solution {
-21    public List<Integer> postorder(Node root) {
-22        List<Integer> result = new ArrayList<>();
-23        traverse(root, result);
-24        return result;
-25    }
-26
-27    private void traverse(Node node, List<Integer> result) {
-28        if (node == null) return;
-29
-30        for (Node child : node.children) {
-31            traverse(child, result);
-32        }
-33
-34        result.add(node.val);
-35    }
-36}
+// Last updated: 28/07/2026, 20:32:50
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+18         if(root1 == null && root2 == null){
+19            return null;
+20        }
+21        if (root1 == null || root2 == null) {
+22            return root1 == null?root2:root1;
+23            
+24        }
+25        TreeNode res = new TreeNode(root1.val+root2.val);
+26        res.left = mergeTrees(root1.left,root2.left);
+27        res.right = mergeTrees(root1.right,root2.right);
+28        return res;
+29    }
+30}
