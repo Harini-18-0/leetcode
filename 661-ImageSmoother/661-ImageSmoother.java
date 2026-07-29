1ae97bee98f2e9713e2e6a3f6fb6a8230e632cb0
Harini <harinivenkatesh96@gmail.com>
2026-07-29 22:07:14 +0530

    File updated at 29/07/2026, 22:07:15

--- 661-ImageSmoother/661-ImageSmoother.java
+++ 661-ImageSmoother/661-ImageSmoother.java
@@ -1,17 +1,11 @@
-// Last updated: 29/07/2026, 22:06:17
-1class MyHashMap {
-2    int[] data;
-3    public MyHashMap() {
-4        data = new int[1000001];
-5        Arrays.fill(data, -1);
-6    }
-7    public void put(int key, int val) {
-8        data[key] = val;
-9    }
-10    public int get(int key) {
-11        return data[key];
-12    }
-13    public void remove(int key) {
-14        data[key] = -1;
-15    }
-16}
+// Last updated: 29/07/2026, 22:07:15
+1class Solution {
+2    public boolean isOneBitCharacter(int[] bits) {
+3        int n = bits.length;
+4        int i = 0;
+5        while(i < n - 1)
+6            i += bits[i] + 1;
+7        return i == n - 1;
+8    }
+9}
+10
