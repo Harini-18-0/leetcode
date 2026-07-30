ed5bf9b193829d30b323afa820a6b83b17d03972
Harini <harinivenkatesh96@gmail.com>
2026-07-30 20:47:51 +0530

    File updated at 30/07/2026, 20:47:51

--- 16-3SumClosest/16-3SumClosest.java
+++ 16-3SumClosest/16-3SumClosest.java
@@ -1,38 +1,33 @@
-// Last updated: 30/07/2026, 20:47:25
+// Last updated: 30/07/2026, 20:47:51
 1class Solution {
-2    public List<String> letterCombinations(String digits) {
-3        List<String> res = new ArrayList<>();
-4        
-5        if (digits == null || digits.length() == 0) {
-6            return res;
-7        }
+2    public ListNode removeNthFromEnd(ListNode head, int n) {
+3        // Handle single node list
+4        if (head.next == null) return null;
+5        
+6        ListNode ptr = head;
+7        ListNode temp = head;
 8        
-9        Map<Character, String> digitToLetters = new HashMap<>();
-10        digitToLetters.put('2', "abc");
-11        digitToLetters.put('3', "def");
-12        digitToLetters.put('4', "ghi");
-13        digitToLetters.put('5', "jkl");
-14        digitToLetters.put('6', "mno");
-15        digitToLetters.put('7', "pqrs");
-16        digitToLetters.put('8', "tuv");
-17        digitToLetters.put('9', "wxyz");
+9        // Move ptr forward n times to create the gap
+10        int i = 1;
+11        while (i <= n) {
+12            ptr = ptr.next; 
+13            i++;
+14        }
+15        
+16        // If ptr becomes null, we need to remove the head
+17        if (ptr == null) return head.next;
 18        
-19        backtrack(digits, 0, new StringBuilder(), res, digitToLetters);
-20        
-21        return res;        
-22    }
-23
-24    private void backtrack(String digits, int idx, StringBuilder comb, List<String> res, Map<Character, String> digitToLetters) {
-25        if (idx == digits.length()) {
-26            res.add(comb.toString());
-27            return;
+19        // Move both until ptr reaches the last node
+20        while (ptr.next != null) {
+21            ptr = ptr.next;
+22            temp = temp.next;
+23        } 
+24        
+25        // Skip the nth node from the end
+26        if (temp.next != null) {
+27            temp.next = temp.next.next;
 28        }
 29        
-30        String letters = digitToLetters.get(digits.charAt(idx));
-31        for (char letter : letters.toCharArray()) {
-32            comb.append(letter);
-33            backtrack(digits, idx + 1, comb, res, digitToLetters);
-34            comb.deleteCharAt(comb.length() - 1);
-35        }
-36    }    
-37}
+30        return head;
+31    }
+32}
