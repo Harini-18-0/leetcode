d112b924e1797bae862c7ad1174c5e38e4221e8f
Harini <harinivenkatesh96@gmail.com>
2026-08-03 21:19:47 +0530

    File updated at 03/08/2026, 21:19:46

--- 45-JumpGameII/45-JumpGameII.java
+++ 45-JumpGameII/45-JumpGameII.java
@@ -1,18 +1,29 @@
-// Last updated: 03/08/2026, 21:19:00
+// Last updated: 03/08/2026, 21:19:46
 1class Solution {
-2    public int jump(int[] nums) {
-3        int near = 0, far = 0, jumps = 0;
-4
-5        while (far < nums.length - 1) {
-6            int farthest = 0;
-7            for (int i = near; i <= far; i++) {
-8                farthest = Math.max(farthest, i + nums[i]);
-9            }
-10            near = far + 1;
-11            far = farthest;
-12            jumps++;
-13        }
-14
-15        return jumps;        
-16    }
-17}
+2    public List<List<Integer>> permute(int[] nums) {
+3        List<List<Integer>> res = new ArrayList<>();
+4        permuteHelper(nums, 0, res);
+5        return res;
+6    }
+7
+8    private void permuteHelper(int[] nums, int i, List<List<Integer>> res) {
+9        if (i == nums.length) {
+10            List<Integer> permutation = new ArrayList<>();
+11            for (int num : nums) permutation.add(num);
+12            res.add(permutation);
+13            return;
+14        }
+15
+16        for (int j = i; j < nums.length; j++) {
+17            swap(nums, i, j);
+18            permuteHelper(nums, i + 1, res);
+19            swap(nums, i, j); // backtrack
+20        }
+21    }
+22
+23    private void swap(int[] nums, int i, int j) {
+24        int tmp = nums[i];
+25        nums[i] = nums[j];
+26        nums[j] = tmp;
+27    }
+28}
