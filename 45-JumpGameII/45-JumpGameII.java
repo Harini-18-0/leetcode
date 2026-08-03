a075da40943271ac4f46cfb999f115b2a25bfb27
Harini <harinivenkatesh96@gmail.com>
2026-08-03 21:21:22 +0530

    File updated at 03/08/2026, 21:21:21

--- 45-JumpGameII/45-JumpGameII.java
+++ 45-JumpGameII/45-JumpGameII.java
@@ -1,25 +1,18 @@
-// Last updated: 03/08/2026, 21:21:08
+// Last updated: 03/08/2026, 21:21:21
 1class Solution {
-2    List<List<Integer>> res=new ArrayList<>();
-3    public List<List<Integer>> permuteUnique(int[] nums) {
-4        Arrays.sort(nums);
-5        List<Integer> list=new ArrayList<>();
-6        boolean[] used=new boolean[nums.length];
-7        permutations(nums,used,list);
-8        return res;
-9    }
-10    public void permutations(int[] nums,boolean[] used,List<Integer>list){
-11        if(list.size()==nums.length){
-12            res.add(new ArrayList<>(list));
-13            return;
-14        }
-15        for(int i=0;i<nums.length;i++){
-16            if( (i>0 && nums[i]==nums[i-1]) && !used[i-1] || (used[i]) ) continue;
-17            list.add(nums[i]);
-18            used[i]=true;
-19            permutations(nums,used,list);
-20            list.remove(list.size()-1);
-21            used[i]=false;
-22        }
-23    }
-24}
+2    public int jump(int[] nums) {
+3        int near = 0, far = 0, jumps = 0;
+4
+5        while (far < nums.length - 1) {
+6            int farthest = 0;
+7            for (int i = near; i <= far; i++) {
+8                farthest = Math.max(farthest, i + nums[i]);
+9            }
+10            near = far + 1;
+11            far = farthest;
+12            jumps++;
+13        }
+14
+15        return jumps;        
+16    }
+17}
