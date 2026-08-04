04b806c7d8377f987da948362465c22e7340b580
Harini <harinivenkatesh96@gmail.com>
2026-08-04 09:02:20 +0530

    File updated at 04/08/2026, 09:02:20

--- 50-Pow(x,n)/50-Pow(x,n).java
+++ 50-Pow(x,n)/50-Pow(x,n).java
@@ -1,25 +1,48 @@
-// Last updated: 04/08/2026, 09:01:51
-1class Solution {
-2    public int[][] insert(int[][] intervals, int[] newInterval) {
-3       List<int[]> intervalList = new ArrayList<>(Arrays.asList(intervals));
-4        intervalList.add(newInterval);
-5        Collections.sort(intervalList, (a, b) -> Integer.compare(a[0], b[0]));
-6
-7        List<int[]> res = new ArrayList<>();
-8        int[] current = intervalList.get(0);
-9
-10        for (int i = 1; i < intervalList.size(); i++) {
-11            int[] interval = intervalList.get(i);
-12            
-13            if (current[1] >= interval[0]) {
-14                current[1] = Math.max(current[1], interval[1]);
-15            } else {
-16                res.add(current);
-17                current = interval;
-18            }
-19        }
-20
-21        res.add(current);
-22        return res.toArray(new int[res.size()][]);
-23    }
-24}
+// Last updated: 04/08/2026, 09:02:20
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode rotateRight(ListNode head, int k) {
+13        if (head == null || head.next == null || k == 0) {
+14            return head;
+15        }
+16
+17        // Step 1: find length and tail
+18        int n = 1;
+19        ListNode tail = head;
+20        while (tail.next != null) {
+21            tail = tail.next;
+22            n++;
+23        }
+24
+25        // Step 2: reduce k
+26        k = k % n;
+27        if (k == 0) {
+28            return head;
+29        }
+30
+31        // Step 3: make circular
+32        tail.next = head;
+33
+34        // Step 4: find new tail
+35        int steps = n - k;
+36        ListNode newTail = head;
+37        for (int i = 1; i < steps; i++) {
+38            newTail = newTail.next;
+39        }
+40
+41        // Step 5: break
+42        ListNode newHead = newTail.next;
+43        newTail.next = null;
+44
+45        return newHead;
+46    }
+47}
